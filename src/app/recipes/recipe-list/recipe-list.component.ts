d5608bfe4d77ea571890ce6566b981e1222e88a6
Tina Dong <Dai.Dong@ibm.com>
2023-12-19 23:42:07 -0400

    commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test', 
+    "This is a sample recipe",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWxF3U763E89Q6Pg4Td9dipNP8VqlA1p6h2w&usqp=CAU"),
+    new Recipe('Test', 
+    "This is a sample recipe",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWxF3U763E89Q6Pg4Td9dipNP8VqlA1p6h2w&usqp=CAU"),
+    
+  ];
 
 }
